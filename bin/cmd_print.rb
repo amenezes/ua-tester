f3d409f213782657d180a0fdffe4333782c1aac6
alexandre menezes <alexandre.fmenezes@gmail.com>
2016-05-02 09:40:26 -0300

    Update cmd_print.rb

--- bin/cmd_print.rb
+++ bin/cmd_print.rb
@@ -19,23 +19,24 @@
 
 module CMDPrint
 
-	def self.print_good( message )
-		puts "\033[00;1;32m[ + ] [ #{ message } ]\033[0m"
-	end
+  def self.print_good( message )
+    puts "\033[00;1;32m[+] [ #{ message } ]\033[0m"
+  end
 
-	def self.print_error( message )
-		puts "\033[00;1;31m[ - ] [ #{ message } ]\033[0m"
-	end
+  def self.print_error( message )
+    puts "\033[00;1;31m[-] [ #{ message } ]\033[0m"
+  end
 
-	def self.print_info( message )
-		puts "\033[00;1;34m[ * ] [ #{ message } ]\033[0m"
-	end
+  def self.print_info( message )
+    puts "\033[00;1;34m[*] [ #{ message } ]\033[0m"
+  end
 
-	def self.print_debug( message )
-		puts "\033[00;1;31m[ * ] [DEBUG] [ #{ message } ]\033[0m"
-	end
+  def self.print_debug( message )
+    puts "\033[00;1;31m[!] [DEBUG] [ #{ message } ]\033[0m"
+  end
 
-	def self.print_version( message )
-		puts "\033[4;34m#{ message }\033[0m"
-	end
+  def self.print_version( message )
+    puts "\033[4;34m#{ message }\033[0m"
+  end
+  
 end
