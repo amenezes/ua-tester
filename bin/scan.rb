ca2da0ffebbf11cd58f3b087e25f1503e7bb9e5f
alexandre menezes <alexandre.fmenezes@gmail.com>
2016-05-02 09:47:59 -0300

    Update scan.rb

--- bin/scan.rb
+++ bin/scan.rb
@@ -16,52 +16,51 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ##
-
 require_relative 'utils'
 require_relative 'cmd_print'
 require_relative 'http_request'
 
 class Scan
 
-	BASE_URL	= "http://localhost"
-	USE_PROXY	= false
+  BASE_URL = "http://localhost"
+  USE_PROXY = false
 
-	def initialize
-		@generic_request = HttpRequest.new
-		@utils = Utils.new
-		@files = @utils.load_signatures_files
-	end
+  def initialize
+    @generic_request = HttpRequest.new
+    @utils = Utils.new
+    @files = @utils.load_signatures_files
+  end
 
-	def use_proxy( use=USE_PROXY, proxy )
-		if use
-			proxy_params = @utils.setting_proxy proxy
-			proxy_addr   = proxy_params[0]
-			proxy_port   = proxy_params[1]
-			@generic_request.proxy proxy_addr, proxy_port
-		end
-	end
+  def use_proxy( use=USE_PROXY, proxy )
+    if use
+      proxy_params = @utils.setting_proxy proxy
+      proxy_addr   = proxy_params[0]
+      proxy_port   = proxy_params[1]
+      @generic_request.proxy proxy_addr, proxy_port
+    end
+  end
 
-	def start_scan( url="#{ BASE_URL }" )
-		begin
-			uri = @utils.normalize url
-			@generic_request.uri uri
+  def start_scan( url="#{ BASE_URL }" )
+    begin
+      uri = @utils.normalize url
+      @generic_request.uri uri
 
-			@files.each do |file|
-				signature_file = YAML.load_file file
-				puts "---"
-				signature_file.each_key do |ua_class|
-					CMDPrint.print_info(ua_class)
-					signature_file[ua_class].each do |ua_value|
-						res = @generic_request.make_request( uri, ua_value )
-					end
-				end
-			end
-			puts ""
-		rescue Interrupt
-			CMDPrint.print_info "Ctrl + C"
-		rescue
- 			puts $!, $@
-			CMDPrint.print_info "Fatal Error!"
-		end
-	end
+      @files.each do |file|
+        signature_file = YAML.load_file file
+        puts "---"
+        signature_file.each_key do |ua_class|
+          CMDPrint.print_info(ua_class)
+          signature_file[ua_class].each do |ua_value|
+            res = @generic_request.make_request( uri, ua_value )
+          end
+        end
+      end
+      puts ""
+    rescue Interrupt
+      CMDPrint.print_info "Ctrl + C"
+    rescue
+      puts $!, $@
+      CMDPrint.print_info "Fatal Error!"
+     end
+  end
 end
