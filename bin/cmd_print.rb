42eb6f201ae64fc79a0b2ce964ec9c8c565f69b9
alexandre menezes <alexandre.fmenezes@gmail.com>
2016-05-02 10:04:43 -0300

    Update cmd_print.rb

--- bin/cmd_print.rb
+++ bin/cmd_print.rb
@@ -19,23 +19,23 @@
 
 module CMDPrint
 
-  def self.print_good( message )
+  def self.print_good(message)
     puts "\033[00;1;32m[+] [ #{ message } ]\033[0m"
   end
 
-  def self.print_error( message )
+  def self.print_error(message)
     puts "\033[00;1;31m[-] [ #{ message } ]\033[0m"
   end
 
-  def self.print_info( message )
+  def self.print_info(message)
     puts "\033[00;1;34m[*] [ #{ message } ]\033[0m"
   end
 
-  def self.print_debug( message )
+  def self.print_debug(message)
     puts "\033[00;1;31m[!] [DEBUG] [ #{ message } ]\033[0m"
   end
 
-  def self.print_version( message )
+  def self.print_version(message)
     puts "\033[4;34m#{ message }\033[0m"
   end
   
